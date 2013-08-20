af8bf8c06a6f8f94f8b7083dd3b5321de2a49f7a
tom kent <tommykent1210@hotmail.com>
2013-08-20 13:46:07 +0100

    Custom openRepo Config

---
+++ app/config/openrepo.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Site Name
+	|--------------------------------------------------------------------------
+	|
+	| The name of your site.
+	|
+	*/
+
+	'sitename' => 'Open Repo Demo Site',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Theme
+	|--------------------------------------------------------------------------
+	|
+	| The theme to use. For use with custom themes. Tells OpenRepo where to
+	| look for templates and CSS. Defaults to "default".
+	|
+	| EG - A "custom" theme
+	| -		"/app/views/custom" should be made, copying templates from 
+	| 		/app/views/default and making any changes
+	| - 	"public/assets/stylesheets/custom" and 
+	|		"public/assets/javascripts/custom" should also be made
+	|
+	*/
+
+	'theme' => 'default',
+
+);
